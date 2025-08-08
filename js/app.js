792220a8b74086d596e8b6537badcb68400a8073
Jorge-Serna <jorge.alfonso.serna@gmail.com>
2025-08-07 22:30:15 -0600

    An API was connected to the app and displays some data and images on HTML

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ function startApp() {
     const categoriesSelect = document.querySelector('#categories');
     categoriesSelect.addEventListener('change', selectCategory)
 
+    const resultsContainer = document.querySelector('#results');
+
     getCategories();
 
     function getCategories() {
@@ -32,15 +34,51 @@ function startApp() {
     function selectCategory(e) {
 
         const cat = e.target.value;
-        const url = `http://www.themealdb.com/api/json/v1/1/filter.php?c=${cat}`
+        const url = `http://www.themealdb.com/api/json/v1/1/filter.php?c=${cat}`;
+
+        // debugger
 
         fetch(url)
             .then(res =>{
                 return res.json();
             })
             .then(result =>{
-                console.log(result)
+                
+                showRecipes(result.meals);
             })
+
+    }
+
+    function showRecipes(recipes = []) {
+
+        const container = document.createElement('div');
+        const row = document.createElement('div');
+        container.classList.add('container');
+        row.classList.add('row', 'g-3');
+
+        container.appendChild(row);
+        
+
+        recipes.forEach( rec =>{
+
+            const col = document.createElement('div');
+            col.classList.add('col-md-4');
+
+            const card = document.createElement('div');
+            card.classList.add('card', 'mb-4');
+
+            col.appendChild(card);
+
+            card.innerHTML = `
+                <img src="${rec.strMealThumb}" class="card-img-top">
+                <div>
+                    hello world
+                </div>
+            `
+            row.appendChild(col)
+        })
+
+        resultsContainer.appendChild(container)
 
     }
 
